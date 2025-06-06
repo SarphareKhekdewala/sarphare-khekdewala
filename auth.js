8efc71753100cb2b12eb78c00fc415187be9603e
SarphareKhekdewala <sarpharekhekdewala@gmail.com>
2025-06-06 22:05:18 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,73 +1,68 @@
-class GoogleAuthManager {
-    constructor() {
-        this.config = {
-            ...window.CONFIG,
-            SCOPES: 'https://www.googleapis.com/auth/spreadsheets',
-            DISCOVERY_DOC: 'https://sheets.googleapis.com/$discovery/rest?version=v4'
-        };
+class GoogleAuth {
+    constructor(config) {
+        this.config = config;
         this.tokenClient = null;
-        // Debug: Log full config
-        console.log('Current Config:', this.config);
+        this.gapiInited = false;
+        this.gisInited = false;
     }
 
-    async initialize() {
-        return new Promise((resolve, reject) => {
-            gapi.load('client', async () => {
-                try {
-                    await gapi.client.init({
-                        apiKey: this.config.API_KEY,
-                        discoveryDocs: [this.config.DISCOVERY_DOC]
-                    });
-
-                    this.tokenClient = google.accounts.oauth2.initTokenClient({
-                        client_id: this.config.CLIENT_ID,
-                        scope: this.config.SCOPES,
-                        callback: ''
-                    });
-
-                    resolve();
-                } catch (error) {
-                    reject(error);
-                }
-            });
+    async init() {
+        await new Promise(resolve => gapi.load('client', resolve));
+        await gapi.client.init({
+            apiKey: this.config.API_KEY,
+            discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
         });
+        this.gapiInited = true;
+
+        this.tokenClient = google.accounts.oauth2.initTokenClient({
+            client_id: this.config.CLIENT_ID,
+            scope: 'https://www.googleapis.com/auth/spreadsheets',
+            callback: '', // Will be set on request
+        });
+        this.gisInited = true;
     }
 
-    async getToken() {
-        if (!this.tokenClient) {
-            await this.initialize();
-        }
-
+    async requestAccessToken() {
         return new Promise((resolve, reject) => {
-            this.tokenClient.callback = (resp) => {
+            this.tokenClient.callback = async (resp) => {
                 if (resp.error) {
                     reject(resp);
-                    return;
+                } else {
+                    resolve(resp);
                 }
-                resolve(resp.access_token);
             };
-
-            if (gapi.client.getToken() === null) {
-                this.tokenClient.requestAccessToken();
-            } else {
-                resolve(gapi.client.getToken().access_token);
-            }
+            this.tokenClient.requestAccessToken({ prompt: '' });
         });
     }
 
-    async saveToSheet(values) {
-        try {
-            await this.getToken();
-            return await gapi.client.sheets.spreadsheets.values.append({
-                spreadsheetId: this.config.SHEET_ID,
-                range: `${this.config.SHEET_NAME}!A:J`,
-                valueInputOption: 'RAW',
-                insertDataOption: 'INSERT_ROWS',
-                resource: { values: [values] }
-            });
-        } catch (error) {
-            console.error('Sheet save failed:', error);
-            throw error;
+    async appendRow(data) {
+        if (!this.gapiInited || !this.gisInited) {
+            await this.init();
         }
+        await this.requestAccessToken();
+
+        const response = await gapi.client.sheets.spreadsheets.values.append({
+            spreadsheetId: this.config.SHEET_ID,
+            range: `${this.config.SHEET_NAME}!A:J`,
+            valueInputOption: 'RAW',
+            insertDataOption: 'INSERT_ROWS',
+            resource: {
+                values: [data]
+            }
+        });
+        return response;
+    }
+
+    async getAllRows() {
+        if (!this.gapiInited || !this.gisInited) {
+            await this.init();
+        }
+        await this.requestAccessToken();
+
+        const response = await gapi.client.sheets.spreadsheets.values.get({
+            spreadsheetId: this.config.SHEET_ID,
+            range: `${this.config.SHEET_NAME}!A:J`,
+        });
+        return response.result.values || [];
     }
 }
