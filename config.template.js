948d9f9c5f6663d221fd151eeb555b7c67f4a8ed
SarphareKhekdewala <sarpharekhekdewala@gmail.com>
2025-06-06 18:05:16 +0000

    Deploying to gh-pages from @ SarphareKhekdewala/sarphare-khekdewala@5c32b045431114525a052cba4a24ffcd7746a767 🚀

---
+++ config.template.js
@@ -0,0 +1,7 @@
+// config.template.js
+const CONFIG = {
+  CLIENT_ID: "#{GOOGLE_CLIENT_ID}#",
+  API_KEY: "#{GOOGLE_API_KEY}#",
+  SHEET_ID: "#{GOOGLE_SHEET_ID}#",
+  SHEET_NAME: "Sarphare Khekdewala Orders"
+};
