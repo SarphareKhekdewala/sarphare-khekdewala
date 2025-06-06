a99b14893e51c983178452be5ec55f784d31fd76
SarphareKhekdewala <sarpharekhekdewala@gmail.com>
2025-06-06 20:20:44 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,37 +1,38 @@
 class GoogleAuthManager {
     constructor() {
         this.config = window.CONFIG;
-        this.tokenClient = null;
     }
 
     async initialize() {
-        await this.loadGAPIClient();
-        await this.initializeGISClient();
-    }
-
-    async loadGAPIClient() {
-        return new Promise((resolve) => {
+        return new Promise((resolve, reject) => {
             gapi.load('client', async () => {
-                await gapi.client.init({
-                    apiKey: this.config.API_KEY,
-                    discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4']
-                });
-                resolve();
-            });
-        });
-    }
+                try {
+                    await gapi.client.init({
+                        apiKey: this.config.API_KEY,
+                        discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4']
+                    });
 
-    async initializeGISClient() {
-        this.tokenClient = google.accounts.oauth2.initTokenClient({
-            client_id: this.config.CLIENT_ID,
-            scope: 'https://www.googleapis.com/auth/spreadsheets',
-            callback: ''
+                    this.tokenClient = google.accounts.oauth2.initTokenClient({
+                        client_id: this.config.CLIENT_ID,
+                        scope: 'https://www.googleapis.com/auth/spreadsheets',
+                        callback: ''
+                    });
+
+                    resolve();
+                } catch (error) {
+                    reject(error);
+                }
+            });
         });
     }
 
     async getToken() {
         return new Promise((resolve) => {
-            this.tokenClient.callback = (resp) => resolve(resp.access_token);
+            this.tokenClient.callback = (response) => {
+                if (response.access_token) {
+                    resolve(response.access_token);
+                }
+            };
             this.tokenClient.requestAccessToken();
         });
     }
