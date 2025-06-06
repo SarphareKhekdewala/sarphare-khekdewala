387282586550049abe3b024d8a017ae849012525
SarphareKhekdewala <sarpharekhekdewala@gmail.com>
2025-06-06 18:06:40 +0000

    Deploying to gh-pages from @ SarphareKhekdewala/sarphare-khekdewala@41b58962fc60e9c8f5f987c73c33cbe16c7097fe 🚀

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.template.js
+const CONFIG = {
+  CLIENT_ID: "83341235106-9nu6r5tn1029a6h7vkhph1rvr6t8ei3m.apps.googleusercontent.com",
+  API_KEY: "AIzaSyAP8fzStJpCmjHgEr0h9PWNIdKIelseen0",
+  SHEET_ID: "1-Y_OUlLf7DjmUvtsNYTIGWouTvn_1R7B3Ml89mrKV2I",
+  SHEET_NAME: "Sarphare Khekdewala Orders"
+};
