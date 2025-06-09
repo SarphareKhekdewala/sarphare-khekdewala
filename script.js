2c275ca740d790317dea0c57058f7eb354911c01
SarphareKhekdewala <sarpharekhekdewala@gmail.com>
2025-06-09 20:34:30 +0000

    Deploying to gh-pages from @ SarphareKhekdewala/sarphare-khekdewala@1bf0047a2a535d0bde7bf473ac078e4d061feb1f 🚀

--- script.js
+++ script.js
@@ -148,4 +148,16 @@ class OrderManager {
     a.download = 'orders.csv';
     a.click();
   }
+  function showTab(tabId) {
+    document.querySelectorAll('.tab-content').forEach(tab => {
+        tab.classList.remove('active');
+    });
+    document.getElementById(tabId).classList.add('active');
+
+    document.querySelectorAll('.tab-btn').forEach(btn => {
+        btn.classList.remove('active');
+    });
+    event.target.classList.add('active');
+}
+
 }
